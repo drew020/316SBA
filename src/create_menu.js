5b7ac8b73310b420e24e87eb0dadc3d7a4b06dc5
Andrew McClain <86428905+drew020@users.noreply.github.com>
2024-02-29 13:11:50 -0500

    edit on menu, still unresponsive????

--- src/create_menu.js
+++ src/create_menu.js
@@ -1,7 +1,3 @@
-const topMenuEl = document.getElementById("top-menu");
-const subMenuEl = document.getElementById("sub-menu");
-const topMenuLinks = [];
-
 const menuLinks = [
     { text: "HOME", href: "/HOME" },
     {
@@ -31,154 +27,252 @@ const menuLinks = [
     },
   ];
 
-  function aLab2() {
-    // set background color to value in css file a variable in the :root
-    topMenuEl.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--top-menu-bg");
+  //import "/src/styles.css";
+
+// reference/cached html elements
+const mainEL = document.querySelector("main");
+const topMenuEl = document.getElementById("top-menu");
+const subMenuEl = document.getElementById("sub-menu");
+const topMenuLinks = [];
+
+//submenu links based on menulink child index
+
+// Java created elements
+const h1 = document.createElement("h1");
+
+/* alab menulink array
+// array for menu items
+const menuLinks = [
+  { text: "about", href: "/about" },
+
+  { text: "catalog", href: "/catalog" },
+
+  { text: "orders", href: "/orders" },
+
+  { text: "account", href: "/account" },
+]; */
+
+//rlab menulink array
+// const menuLinks = [
+//   { text: "about", href: "/about" },
+//   {
+//     text: "catalog",
+//     href: "#",
+//     subLinks: [
+//       { text: "all", href: "/catalog/all" },
+//       { text: "top selling", href: "/catalog/top" },
+//       { text: "search", href: "/catalog/search" },
+//     ],
+//   },
+//   {
+//     text: "orders",
+//     href: "#",
+//     subLinks: [
+//       { text: "new", href: "/orders/new" },
+//       { text: "pending", href: "/orders/pending" },
+//       { text: "history", href: "/orders/history" },
+//     ],
+//   },
+//   {
+//     text: "account",
+//     href: "#",
+//     subLinks: [
+//       { text: "profile", href: "/account/profile" },
+//       { text: "sign out", href: "/account/signout" },
+//     ],
+//   },
+// ];
+
+function aLab1() {
+  // style is the attribute, background color is the property of style attribute
+  // set background color to value
+  mainEL.style.backgroundColor = getComputedStyle(
+    // get document root because the variable we seek is
+    //declared and initialized there in css file.
+    document.documentElement
+    // gets css varible, "--main-bg", value from :root selector in css
+  ).getPropertyValue("--main-bg");
+  //get document.body through stored reference variable mainEL,
+  //create a child element within it; a h1. Get the newly made
+  //child(h1) and insert a text node with "DOM Manipulation" value inside.
+  mainEL.appendChild(h1);
+  h1.textContent = "DOM Manipulation";
+  // get mainEL again to add class to body element in html so it may inherit its CSS properties.
+  mainEL.setAttribute("class", "flex-ctr");
+}
+
+function aLab2() {
+  // set height to 100%.
+  topMenuEl.style.height = "100%";
+  // set background color to value.
+  topMenuEl.style.backgroundColor = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--top-menu-bg");
+  // add class
+  topMenuEl.setAttribute("class", "flex-around");
+}
+
+/* function alab3() {
+  // iterate over menuLinks array.
+  for (let i = 0; i < menuLinks.length; i++) {
+    // created element hyperling element
+    const menuLinkEl = document.createElement("a");
+
+    // edit hyperlink text
+    menuLinkEl.textContent = menuLinks[i].text;
+
+    // adds hyperling reference to a element
+    menuLinkEl.setAttribute("href", menuLinks[i].href);
+
+    // adds the generated a element to menu
+    topMenuEl.appendChild(menuLinkEl);
   }
-  function rLab3() {
-    //set height
-    subMenuEl.style.height = "100%";
-  
-    //set submenu bg to css stored submenu bg color
-    subMenuEl.style.backgroundColor = getComputedStyle(
-      document.documentElement
-    ).getPropertyValue("--sub-menu-bg");
-  
-    //subMenuEl.setAttribute("class", "flex-around");
-  
-    //   Set the CSS position property of subMenuEl to the value of absolute.
-    subMenuEl.style.position = "absolute";
-  
-    // Set the CSS top property of subMenuEl to the value of 0.
-    subMenuEl.style.top = 0;
+}
+ */
+
+function rLab3() {
+  //set height
+  subMenuEl.style.height = "100%";
+
+  //set submenu bg to css stored submenu bg color
+  subMenuEl.style.backgroundColor = getComputedStyle(
+    document.documentElement
+  ).getPropertyValue("--sub-menu-bg");
+
+  subMenuEl.setAttribute("class", "flex-around");
+
+  //   Set the CSS position property of subMenuEl to the value of absolute.
+  subMenuEl.style.position = "absolute";
+
+  // Set the CSS top property of subMenuEl to the value of 0.
+  subMenuEl.style.top = 0;
+}
+
+function rLab4() {
+  // iterate over menuLinks array.
+  for (let i = 0; i < menuLinks.length; i++) {
+    // created element hyperling element
+    const menuLinkEl = document.createElement("a");
+    // edit hyperlink text
+    menuLinkEl.textContent = menuLinks[i].text;
+    // adds hyperling reference to a element
+    menuLinkEl.setAttribute("href", menuLinks[i].href);
+    // adds the generated a element to menu
+    topMenuEl.appendChild(menuLinkEl);
+    // stores a cache reference topmenulinks array
+    topMenuLinks.push(topMenuEl.childNodes[i]);
   }
-  
-  function rLab4() {
-    // iterate over menuLinks array.
-    for (let i = 0; i < menuLinks.length; i++) {
-      // created element hyperling element
-      const menuLinkEl = document.createElement("a");
-      // edit hyperlink text
-      menuLinkEl.textContent = menuLinks[i].text;
-      // adds hyperling reference to a element
-      menuLinkEl.setAttribute("href", menuLinks[i].href);
-      // adds the generated a element to menu
-      topMenuEl.appendChild(menuLinkEl);
-      // stores a cache reference topmenulinks array
-      topMenuLinks.push(topMenuEl.childNodes[i]);
-    }
-  }
-  
-  function rLab5() {
-    // Add click event listener to the parent
-    topMenuEl.addEventListener("click", function (event) {
-      // Prevent default behavior
-      event.preventDefault();
-      // Check if the clicked element is an <a> element
-      if (event.target.tagName === "A") {
-        const l_claas = event.target.classList;
-        const l_active = document.getElementsByClassName("active");
-        if (
-          event.target.textContent != "about" &&
-          l_claas.length == 0 &&
-          l_active.length == 0
-        ) {
-          deletechildrenEl();
-  
-          //populates submenulink
-          subMenuEl.style.top = "100%";
-  
-          let j;
-          let n;
-          if (event.target.textContent == "GAMES") {
-            j = 1;
-            n = 2;
-            populatesubmenu(j, n);
-          } else if (event.target.textContent == "NEWS") {
-            j = 2;
-            n = 3;
-            populatesubmenu(j, n);
-          } else if (event.target.textContent == "ACCOUNT") {
-            j = 3;
-            n = 2;
-            populatesubmenu(j, n);
-          }
-  
-          //add active class to clicked link
-          event.target.setAttribute("class", "active");
-        } else if (
-          event.target.textContent != "HOME" &&
-          l_claas.length == 0 &&
-          l_active.length > 0
-        ) {
-          deletechildrenEl();
-  
-          let j;
-          let n;
-          if (event.target.textContent == "GAMES") {
-            j = 1;
-            n = 2;
-            populatesubmenu(j, n);
-          } else if (event.target.textContent == "NEWS") {
-            j = 2;
-            n = 3;
-            populatesubmenu(j, n);
-          } else if (event.target.textContent == "ACCOUNT") {
-            j = 3;
-            n = 2;
-            populatesubmenu(j, n);
-          }
-          //remvoe active class from all links
-          topMenuLinks.forEach(function (element) {
-            element.classList.remove("active");
-          });
-          // set active
-          event.target.setAttribute("class", "active");
-        } else {
-          //remvoe active class from all links
-          topMenuLinks.forEach(function (element) {
-            element.classList.remove("active");
-          });
-          // hide submenu
-          subMenuEl.style.top = "0%";
-        }
-      } else {
-        subMenuEl.style.top = "0%";
-  
+}
+
+function rLab5() {
+  // Add click event listener to the parent
+  topMenuEl.addEventListener("click", function (event) {
+    // Prevent default behavior
+    event.preventDefault();
+    // Check if the clicked element is an <a> element
+    if (event.target.tagName === "A") {
+      const l_claas = event.target.classList;
+      const l_active = document.getElementsByClassName("active");
+      if (
+        event.target.textContent != "about" &&
+        l_claas.length == 0 &&
+        l_active.length == 0
+      ) {
         deletechildrenEl();
-  
+
+        //populates submenulink
+        subMenuEl.style.top = "100%";
+
+        let j;
+        let n;
+        if (event.target.textContent == "catalog") {
+          j = 1;
+          n = 3;
+          populatesubmenu(j, n);
+        } else if (event.target.textContent == "orders") {
+          j = 2;
+          n = 3;
+          populatesubmenu(j, n);
+        } else if (event.target.textContent == "account") {
+          j = 3;
+          n = 2;
+          populatesubmenu(j, n);
+        }
+
+        //add active class to clicked link
+        event.target.setAttribute("class", "active");
+      } else if (
+        event.target.textContent != "about" &&
+        l_claas.length == 0 &&
+        l_active.length > 0
+      ) {
+        deletechildrenEl();
+
+        let j;
+        let n;
+        if (event.target.textContent == "catalog") {
+          j = 1;
+          n = 3;
+          populatesubmenu(j, n);
+        } else if (event.target.textContent == "orders") {
+          j = 2;
+          n = 3;
+          populatesubmenu(j, n);
+        } else if (event.target.textContent == "account") {
+          j = 3;
+          n = 2;
+          populatesubmenu(j, n);
+        }
+        //remvoe active class from all links
+        topMenuLinks.forEach(function (element) {
+          element.classList.remove("active");
+        });
+        // set active
+        event.target.setAttribute("class", "active");
+      } else {
         //remvoe active class from all links
         topMenuLinks.forEach(function (element) {
           element.classList.remove("active");
         });
         // hide submenu
+        subMenuEl.style.top = "0%";
       }
-      //debug onclicked link
-      console.log("Clicked link content:", event.target.textContent);
-    });
-  }
-  function populatesubmenu(index, num) {
-    for (let i = 0; i < num; i++) {
-      // created element hyperling element
-      const menuLinkEl = document.createElement("a");
-      // edit hyperlink text
-      menuLinkEl.textContent = menuLinks[index].subLinks[i].text;
-      // adds hyperling reference to a element
-      menuLinkEl.setAttribute("href", menuLinks[index].subLinks[i].href);
-      // adds the generated a element to menu
-      subMenuEl.appendChild(menuLinkEl);
-    }
-  }
-  function deletechildrenEl() {
-    while (subMenuEl.firstChild) {
-      subMenuEl.removeChild(subMenuEl.firstChild);
-    }
-  }
+    } else {
+      subMenuEl.style.top = "0%";
 
+      deletechildrenEl();
+
+      //remvoe active class from all links
+      topMenuLinks.forEach(function (element) {
+        element.classList.remove("active");
+      });
+      // hide submenu
+    }
+    //debug onclicked link
+    console.log("Clicked link content:", event.target.textContent);
+  });
+}
+function populatesubmenu(index, num) {
+  for (let i = 0; i < num; i++) {
+    // created element hyperling element
+    const menuLinkEl = document.createElement("a");
+    // edit hyperlink text
+    menuLinkEl.textContent = menuLinks[index].subLinks[i].text;
+    // adds hyperling reference to a element
+    menuLinkEl.setAttribute("href", menuLinks[index].subLinks[i].href);
+    // adds the generated a element to menu
+    subMenuEl.appendChild(menuLinkEl);
+  }
+}
+function deletechildrenEl() {
+  while (subMenuEl.firstChild) {
+    subMenuEl.removeChild(subMenuEl.firstChild);
+  }
+}
+aLab1();
 aLab2();
+//alab3); Deprecated
 rLab3();
 rLab4();
 rLab5();
-  
+// Extract sublinks
--- src/script.js
+++ src/script.js
@@ -1,138 +1,122 @@
-// reference/cached html elements
+const f_name = document.forms["myForm"]["fname"];
+const f_sex = document.forms["myForm"]["fsex"];
+const f_class = document.forms["myForm"]["fclass"];
+const f_element = document.forms["myForm"]["felement"];
+const f_point = document.forms["myForm"]["fpoint"];
 
+let f_point_stored = document.forms["myForm"]["fpoint"].value;
 
+const a_Attribute_Element = [];
+a_Attribute_Element[0] = document.forms["myForm"]["fstrength"];
+a_Attribute_Element[1] = document.forms["myForm"]["fintelligence"];
+a_Attribute_Element[2] = document.forms["myForm"]["fagility"];
+a_Attribute_Element[3] = document.forms["myForm"]["fwisdom"];
 
-    const f_name = document.forms["myForm"]["fname"];
-    const f_sex = document.forms["myForm"]["fsex"];
-    const f_class = document.forms["myForm"]["fclass"];
-    const f_element = document.forms["myForm"]["felement"];
-    const f_point = document.forms["myForm"]["fpoint"];
-    const f_strength = document.forms["myForm"]["fstrength"];
-    const f_intelligence = document.forms["myForm"]["fintelligence"];
-    const f_agility = document.forms["myForm"]["fagility"];
-    const f_wisdom = document.forms["myForm"]["fwisdom"];
+const a_Attribute_Stored = [];
+a_Attribute_Stored[0] = a_Attribute_Element[0];
+a_Attribute_Stored[1] = a_Attribute_Element[1];
+a_Attribute_Stored[2] = a_Attribute_Element[2];
+a_Attribute_Stored[3] = a_Attribute_Element[3];
 
-    let f_point_stored = document.forms["myForm"]["fpoint"].value;
-    let f_strength_stored = document.forms["myForm"]["fstrength"].value;
-    let f_intelligence_stored = document.forms["myForm"]["fintelligence"].value;
-    let f_agility_stored = document.forms["myForm"]["fagility"].value;
-    let f_wisdom_stored = document.forms["myForm"]["fwisdom"].value;
-  // Add event listener for increment button
- //f_strength.addEventListener('wheel',  stepeEventwheel, false);
- //f_strength.addEventListener('input',  stepeEventclick);
+// loop through the attribut array to add eventlisterner to each attribute
+for(let i = 0; i > a_Attribute_Element.length;i++)
+{
+  a_Attribute_Element[i].addEventListener('input', stepeEventInput);
+  console.log(`${a_Attribute_Element[i].name} stepEventinput listening`)
+  resetAttributePoints();
+}
 
-f_strength.addEventListener('change',  (event) =>{
+function stepeEventInput(attribute) {
+let l_index = 0;
 
- if(f_point.value > 0){
-  if(f_point.value - f_strength.value > 0){
-    f_point.value = f_point.value - (f_strength.value - f_strength_stored);
-    f_strength_stored = f_strength.value;
-    console.log(f_strength_stored);
+  // Which attribute index is it?
+  for(let i = 0; )
+  //check if html is in inital state.
+  if (f_point.value == 40 && attribute.target.value == 0) {
+    f_point.value = f_point.value - (attribute.target.value - f_strength_stored);
   }
-  else{
-  }
-    
-  }
-  else{
-    f_strength.value = 1;
-  }
-
-  console.log(`input event on ${event.type} 2`)
-
-},false);
-
-
-
-
-
-  function stepeEventclick(event) {
-        event.preventDefault();
-    console.log(`click event on ${event.type.myForm.value} `)
-  }
-  function stepeEventwheel(event){
-
-    if (event.deltaY > 0) {
-      // Decrease value
-      this.stepDown();
-    } else {
-      // Increase value
-      this.stepUp();
+  else
+    if (f_point.value < 40 && f_point.value > 0) {
+      f_point.value = f_point.value - (f_strength.value - f_strength_stored);
     }
-    
-    // Prevent default scrolling behavior
-    event.preventDefault();
-    console.log(`wheel event on ${event.type}`)
-
-  };
-
-  function stepUp(){
-    console.log("stepUp");
+    else {
+      //attribute = 
   }
-  function stepDown(){
-    console.log("stepDown");
+  // if (f_point.value - f_strength.value > 0) {
+  //   f_point.value = f_point.value - (f_strength.value - f_strength_stored);
+  //   console.log(f_strength_stored);
+  // }
+  // else {
+  //   f_strength.value = f_strength_stored;
+  // }
+
+  f_strength_stored = f_strength.value;
+  console.log(`input event on ${event.type}`)
+}
+function stepeEventwheel(event) {
+
+  if (event.deltaY > 0) {
+    // Decrease value
+    this.stepDown();
+  } else {
+    // Increase value
+    this.stepUp();
   }
-//#region Dialogue literals
-/* const dialogueArrText = [];
-const dialogueArrTag = [];
 
-dialogueArr[0] = "What is your name?";
-dialogueArr[1] = "A magical item swells with energy in your hands. ";
-dialogueArr[2] = "Distribute your attributes.";
-dialogueArr[3] =
-  "In the heart of an ancient yet bustling city of Arcanum, where magic flowed like a hidden river, lived an eccentric alchemist. their workshop was a cluttered haven of bubbling flasks dusty tomes, and peculiar artifacts. But their most prized possession sat atop a wooden pedestal—an intricately etched glass jar. They believed that within this jar lay the essence of forgotten realms—a microcosm of existence. it is called the “Dungeon Jar,” and its secrets whispered to her in dreams."; */
-//#endregion Dialogue
+  // Prevent default scrolling behavior
+  event.preventDefault();
+  console.log(`wheel event on ${event.type}`)
 
-// Java created elements
+};
 
-//#region Menu stuff
-// Menu links posed as a easily editable object to be generated via a loop.
-
-
-
-
-//#endregion Menu stuff
-
-//#region Menu stuff
+function stepUp() {
+  console.log("stepUp");
+}
+function stepDown() {
+  console.log("stepDown");
+}
 
 function validateForm() {
 
   try {
-      if (f_name.value.length == 0 && f_name.value.length > 12) {
-    alert("Name must be filled out and less than 12 character");
-    return false;
-  }
+    if (f_name.value.length == 0 && f_name.value.length > 12) {
+      alert("Name must be filled out and less than 12 character");
+      return false;
+    }
 
-  if ((f_strength.value + f_intelligence.value + f_agility.value + f_wisdom.value) != 40){
-    alert("Most use all 40 Attribute points");
-    return false;
-  }
+    if ((f_strength.value + f_intelligence.value + f_agility.value + f_wisdom.value) != 40) {
+      alert("Most use all 40 Attribute points");
+      return false;
+    }
   } catch (error) {
     console.log("Validation failed.")
   }
 
 }
 
-function updateAttributePoints(){
+function updateAttributePoints() {
 
   try {
-      f_point = 40 - (f_strength+f_intelligence+f_wisdom+f_agility);
+    f_point = 40 - (f_strength.value + f_intelligence.value + f_wisdom.value + f_agility.value);
   } catch (error) {
     console.log(" Can not updateAttributePoints")
   }
-
+  console.log(`Attribut point value updated.`)
   return f_point;
 }
 
-function resetAttributePoints(){
-f_point.value = 36;
-f_strength.value = 1;
-f_intelligence.value = 1;
-f_wisdom.value = 1;
-f_agility.value = 1;
+function resetAttributePoints() {
+  f_point.value = 40;
+
+  f_strength.value = 0;
+  f_strength_stored = 0;
+
+  f_intelligence.value = 0;
+  f_intelligence_stored = 0;
+
+  f_wisdom.value = 0;
+  f_wisdom_stored;
+
+  f_agility.value = 0;
+  f_agility_stored = 0
 }
-
-
-//#endregion Menu stuff
-
-
-// Extract sublinks
